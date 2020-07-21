5955ba873d2ad1661e2b7a5a918b5a1372c25c95
villa999 <vietanh.hn.4078@gmail.com>
2020-07-21 23:30:47 +0700

    fix mongoDB

--- src/libs/mongo-db.js
+++ src/libs/mongo-db.js
@@ -3,8 +3,8 @@ require("../apps/models/category")
 require("../apps/models/product")
 require("../apps/models/user")
 require("../apps/models/comment")
-const uris = "mongodb://127.0.0.1:27017/Vietpro_mongodb"
-//const uris = process.env.MONGODB_URI
+//const uris = "mongodb://127.0.0.1:27017/Vietpro_mongodb"
+const uris = process.env.MONGODB_URI
 //const uris = "mongodb://mongodb/Vietpro_mongodb"
 
 //uris: link connect toi mongodb
